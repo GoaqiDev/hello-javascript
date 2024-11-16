948cb330d3ce74e8cedd56154f15d5a802ecd5d6
GoaqiDev <goaqidev@gmail.com>
2024-11-16 16:19:58 +0100

    cambios

--- Basic/21-destructuring-spreading-exercises.js
+++ Basic/21-destructuring-spreading-exercises.js
@@ -4,23 +4,100 @@ Objetos, desestructuración, propagación y clases
 https://www.youtube.com/live/SBXEpAx_y_Q?si=Sbqqa2-_eGwsUkH-&t=958
 */
 
-// 1. Usa desestructuración para extraer los dos primeros elementos de un array 
+let myArray = [1, 2, 3, 4];
+
+// 1. Usa desestructuración para extraer los dos primeros elementos de un array
+
+let [elementoUno, elementoDos] = myArray;
 
 // 2. Usa desestructuración en un array y asigna un valor predeterminado a una variable
 
+let [
+  primerElemento,
+  segundoElemento,
+  tercerElemento,
+  cuartoElemento = "Elemento por defecto",
+] = myArray;
+
 // 3. Usa desestructuración para extraer dos propiedades de un objeto
+
+let [, unEle, , dosEle] = myArray;
 
 // 4. Usa desestructuración para extraer dos propiedades de un objeto y asígnalas
 //    a nuevas variables con nombres diferentes
 
+let [ej4_1, ej4_2, ,] = myArray;
+
+let otroNombre1 = ej4_1;
+
+let otroNombre2 = ej4_2;
+
+console.log(otroNombre1); //Imprime 1
+
 // 5. Usa desestructuración para extraer dos propiedades de un objeto anidado
+
+let persona = {
+  nombre: "Joaquin",
+  apellido: "Gutierrez",
+  edad: 37,
+  oficio: {
+    nombre: "Policia",
+    años_experiencia: 10,
+  },
+};
+
+let { nombre: elNombre, años_experiencia } = persona.oficio;
+
+console.log(elNombre);
+console.log(años_experiencia);
 
 // 6. Usa propagación para combinar dos arrays en uno nuevo
 
+let arrayUno = [1,2,3];
+let arrayDos = [4,5,6];
+
+let arrayPropagado = [...arrayUno, ...arrayDos];
+
+console.log(arrayPropagado)
+
 // 7. Usa propagación para crear una copia de un array
+
+let arrayOriginal = [1,2,3,4,5,6]
+let arrayCopia = [...arrayOriginal]
+console.log(arrayCopia)
 
 // 8. Usa propagación para combinar dos objetos en uno nuevo
 
+let objetoUno = {
+  nombre: "Joaquin",
+  edad: 37
+}
+
+let objetoDos = {
+  profesión: "Policía",
+  estudios: "informático"
+}
+
+let objetoFinal = {...objetoUno, ...objetoDos}
+
+console.log(objetoFinal)
+
 // 9. Usa propagación para crear una copia de un objeto
 
+let objetoOriginal = {
+  nombre: "Joaquin",
+  edad: 37,
+  profesión: "Policía",
+  estudios: "Ingeniero en Informática"
+}
+
+let objetoCopia = {...objetoOriginal}
+console.log(objetoCopia)
+
 // 10. Combina desestructuración y propagación
+
+let arrayFinal1 = [1,2,3,4,5,6]
+let arrayFinal2 = ["a","b","c","d"]
+
+let 
+
--- Basic/23-classes-exercises.js
+++ Basic/23-classes-exercises.js
@@ -6,20 +6,93 @@ https://www.youtube.com/live/8p6SLAIgwZI?si=MS9o7qIhezx9NTQX&t=902
 
 // 1. Crea una clase que reciba dos propiedades
 
-// 2. Añade un método a la clase que utilice las propiedades
+class PrimerEjercicio {
+  #nombre;
+  #apellido;
 
+  constructor(nombre, apellido) {
+    this.#nombre = nombre;
+    this.#apellido = apellido;
+  }
+
+  get nombre() {
+    return this.#nombre;
+  }
+
+  get apellido() {
+    return this.#apellido;
+  }
+
+  set nombre(newNombre) {
+    this.#nombre = newNombre;
+  }
+
+  set apellido(newApellido) {
+    this.#apellido = newApellido;
+  }
+
+  //Solución del ejercicio 4
+  static funcionEstatica() {
+    console.log("Esto es una función estática");
+  }
+
+  // 2. Añade un método a la clase que utilice las propiedades
+
+  imprime() {
+    console.log(this.#nombre + " " + this.#apellido);
+  }
+}
+
+let person = new PrimerEjercicio("Joaquín", "Gutiérrez");
 // 3. Muestra los valores de las propiedades e invoca a la función
+
+person.imprime();
 
 // 4. Añade un método estático a la primera clase
 
 // 5. Haz uso del método estático
 
+PrimerEjercicio.funcionEstatica();
+
 // 6. Crea una clase que haga uso de herencia
 
+class OtraClase extends PrimerEjercicio {
+  constructor(oficio, edad) {
+    this.oficio = oficio;
+    this.edad = edad;
+  }
+}
+
 // 7. Crea una clase que haga uso de getters y setters
+
+class ClaseSiete {
+  #ejemplo;
+
+  constructor(ejemplo) {
+    this.#ejemplo = ejemplo;
+  }
+
+  get ejemplo() {
+    return this.#ejemplo;
+  }
+
+  set ejemplo(newEjemplo) {
+    this.#ejemplo = newEjemplo;
+  }
+
+  RenuevaEjemplo(newEj) {
+    ejemplo(newEj);
+  }
+}
+
+const eJ = new ClaseSiete("Antonio");
+
+console.log(eJ.ejemplo);
+eJ.ejemplo = "Joaquin";
+console.log(eJ.ejemplo);
 
 // 8. Modifica la clase con getters y setters para que use propiedades privadas
 
 // 9. Utiliza los get y set y muestra sus valores
 
-// 10. Sobrescribe un método de una clase que utilice herencia 
+// 10. Sobrescribe un método de una clase que utilice herencia
