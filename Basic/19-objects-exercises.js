cddac05c9fba78202e53aec7c94def8f2577b8c4
jgutierrezgil <joaquingutierrezgil@gmail.com>
2025-01-01 05:24:58 +0100

    mas exer

--- Basic/19-objects-exercises.js
+++ Basic/19-objects-exercises.js
@@ -6,20 +6,53 @@ https://www.youtube.com/live/SBXEpAx_y_Q?si=Sbqqa2-_eGwsUkH-&t=958
 
 // 1. Crea un objeto con 3 propiedades
 
+let obj1 = {
+    prop1: "Propiedad 1",
+    prop2: "Propiedad 2",
+    prop3: "Propiedad 3"
+}
+
 // 2. Accede y muestra su valor
 
+console.log(obj1)
+console.log(obj1.prop1)
+
 // 3. Agrega una nueva propiedad
+obj1.prop4 = "Propiedad 4"
+console.log(obj1)
 
 // 4. Elimina una de las 3 primeras propiedades
+delete obj1.prop4
+console.log(obj1)
 
 // 5. Agrega una función e invócala
+obj1.myFunc = function(){
+    console.log("Función dentro del objeto")
+}
+console.log(obj1)
 
 // 6. Itera las propiedades del objeto
+for(let key in obj1){
+    console.log(key)
+}
 
 // 7. Crea un objeto anidado
+let objAnidado = {
+    nombre: "Joaquin",
+    edad: 37,
+    objInterno: {
+        trabajo: "Policía"
+    }
+}
+console.log(objAnidado)
+
+console.log(objAnidado.objInterno.trabajo)
 
 // 8. Accede y muestra el valor de las propiedades anidadas
+console.log(objAnidado.objInterno.trabajo)
 
 // 9. Comprueba si los dos objetos creados son iguales
+console.log(objAnidado == objAnidado.objInterno)
 
 // 10. Comprueba si dos propiedades diferentes son iguales
+console.log(objAnidado.nombre == objAnidado.objInterno.trabajo)
--- Basic/25-error-handling-exercises.js
+++ Basic/25-error-handling-exercises.js
@@ -6,13 +6,70 @@ https://www.youtube.com/live/8p6SLAIgwZI?si=MS9o7qIhezx9NTQX&t=902
 
 // 1. Captura una excepción utilizando try-catch
 
+let myObject1
+
+try {
+    console.log(myObject.age)
+} catch (error){
+    console.log("Error capturado: ", error)
+}
+
 // 2. Captura una excepción utilizando try-catch y finally
+
+let myObject2
+
+try {
+    console.log(myObject2.age)
+} catch (error){
+    console.log("Error capturado: ", error)
+} finally {
+    console.log("El objeto no tiene parámetros")
+}
 
 // 3. Lanza una excepción genérica
 
+//throw new Error(console.log("Excepción genérica"))
+
 // 4. Crea una excepción personalizada
 
+class MyException extends Error{
+    constructor(message){
+        super(message)
+        this.name = "Custom Exception"
+    }
+}
+
+try{
+    throw new MyException("Este es el error lanzado")
+} catch (error){
+    console.log("Error en el catch: ", error.name)
+    console.log("Error en el catch: ", error.message)
+}
+
 // 5. Lanza una excepción personalizada
+
+class MiErrorPersonalizado extends Error {
+    constructor(message) {
+        super(message);
+        this.name = "MiErrorPersonalizado";
+    }
+}
+
+function verificaEdad(edad) {
+    if (edad < 18) {
+        throw new MiErrorPersonalizado("Debes tener al menos 18 años para continuar.");
+    }
+    return "Acceso permitido.";
+}
+
+// Ejemplo de uso:
+try {
+    console.log(verificaEdad(16)); // Esto lanzará la excepción
+} catch (error) {
+    console.log(error.name); // "MiErrorPersonalizado"
+    console.log(error.message); // "Debes tener al menos 18 años para continuar."
+}
+
 
 // 6. Lanza varias excepciones según una lógica definida
 
@@ -23,3 +80,7 @@ https://www.youtube.com/live/8p6SLAIgwZI?si=MS9o7qIhezx9NTQX&t=902
 // 9. Crea una función que verifique si un objeto tiene una propiedad específica y lance una excepción personalizada
 
 // 10. Crea una función que realice reintentos en caso de error hasta un máximo de 10
+
+function repe(){
+    
+}
